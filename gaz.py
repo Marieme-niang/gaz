9edcdffa4c5e96b4f7e11d023bf335dd00802f86
Marieme-niang <niang.marieme2003@gmail.com>
2024-10-10 22:44:40 +0200

    Update gaz.py

--- gaz.py
+++ gaz.py
@@ -1,73 +1,11 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection  import train_test_split 
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import seaborn as sns
 
 
 
-df = pd.read_csv("Gas_Sensors_Measurements.csv")
-
-
-# Initialiser le LabelEncoder
-encoder = LabelEncoder()
-# Encodage de la colonne 'Gas'
-df['Gas_encoded'] = encoder.fit_transform(df['Gas'])
-# Encodage de la colonne 'Corresponding Image Name'
-df['Image_Name_encoded'] = encoder.fit_transform(df['Corresponding Image Name'])
-
-
-#encoder = LabelEncoder()
-
-# Initialiser le LabelEncoder
-encoder = LabelEncoder()
-# Encodage de la colonne 'Gas'
-df['Gas'] = encoder.fit_transform(df['Gas'])
-
-
-# Initialiser le LabelEncoder
-encoder = LabelEncoder()
-# Encodage de la colonne 'Corresponding Image Name'
-df['Image_Name_encoded'] = encoder.fit_transform(df['Corresponding Image Name'])
-
-# Initialiser le LabelEncoder
-label_encoder = LabelEncoder()
-# Appliquer l'encodage sur la colonne 'Category'
-df['Corresponding Image Name'] = label_encoder.fit_transform(df['Corresponding Image Name'])
-
-# Utilisez drop() avec des parenthèses
-x = df.drop(columns=['Corresponding Image Name'])
-
-y = df['Corresponding Image Name']
-
-x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2 , random_state=42)
-
-
-scaler = MinMaxScaler()
-
-# Sélectionner uniquement les colonnes numériques pour la normalisation
-numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
-
-# Appliquer la normalisation sur les colonnes numériques
-df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
-
-
-model = LinearRegression()
-model.fit(x_test , y_test)
-# Prédictions pour la ligne de régression
-predictions = model.predict(x)
-
-# Calculer les métriques
-mse = mean_squared_error(y, predictions)
-mae = mean_absolute_error(y, predictions)
-r2 = r2_score(y, predictions)
-
+df = pd.read_csv("Gas_Sensors_Measurements.csv
 
 
 
